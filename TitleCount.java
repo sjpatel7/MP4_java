221675c00cca71660bf41cab62e947faeb78026a
sjpatel7 <31695897+sjpatel7@users.noreply.github.com>
2020-03-24 05:13:30 -0500

    Update wordcount map and reduce (using TopWords)

--- TitleCount.java
+++ TitleCount.java
@@ -87,6 +87,14 @@ public class TitleCount extends Configured implements Tool {
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             //TODO
+            String line = value.toString();
+			StringTokenizer tokenizer = new StringTokenizer(line, this.delimiters);
+			while (tokenizer.hasMoreTokens()) {
+				String nextToken = tokenizer.nextToken();
+				if (!this.stopWords.contains(nextToken.trim().toLowerCase())) {
+					context.write(new Text(nextToken), new IntWritable(1));
+				}
+			}
             //context.write(<Text>, <IntWritable>); // pass this output to reducer
         }
     }
@@ -95,6 +103,11 @@ public class TitleCount extends Configured implements Tool {
         @Override
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             //TODO
+            int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			context.write(key, new IntWritable(sum));
             //context.write(<Text>, <NullWritable>); // print as final output
         }
     }
