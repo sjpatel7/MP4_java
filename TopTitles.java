0cace2f976bf73a9224b623785b4661e7e978e41
sjpatel7 <31695897+sjpatel7@users.noreply.github.com>
2020-03-24 05:56:36 -0500

    lower case only

--- TopTitles.java
+++ TopTitles.java
@@ -128,8 +128,8 @@ public class TopTitles extends Configured implements Tool {
             String line = value.toString();
             StringTokenizer tokenizer = new StringTokenizer(line, this.delimiters);
             while (tokenizer.hasMoreTokens()) {
-                String nextToken = tokenizer.nextToken();
-                if (!this.stopWords.contains(nextToken.trim().toLowerCase())) {
+                String nextToken = tokenizer.nextToken().toLowerCase();
+                if (!this.stopWords.contains(nextToken.trim())) {
                     context.write(new Text(nextToken), new IntWritable(1));   
                 }
             }
